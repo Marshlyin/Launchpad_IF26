5e3fed03ab2e6d18a816fb36b65b942d37da2f93
user148557 <user148557@92-63-141-194.rdns.melbourne.co.uk>
2018-12-28 12:35:41 -0800

    Initial Commit

---
+++ Launchpad_IF26/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Launchpad_IF26
+//
+//  Created by user148557 on 28/12/2018.
+//  Copyright © 2018 user148557. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
