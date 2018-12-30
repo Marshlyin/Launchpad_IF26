86ca4142ff1c1e18c95f6f594e8a259b77147032
user148557 <user148557@92-63-141-194.rdns.melbourne.co.uk>
2018-12-30 14:36:00 -0800

    Ajout des sons dans le projet

---
+++ Launchpad_IF26/DrumpadLoFi.swift
@@ -0,0 +1,20 @@
+//
+//  DrumpadLoFi.swift
+//  Launchpad_IF26
+//
+//  Created by user148557 on 30/12/2018.
+//  Copyright © 2018 user148557. All rights reserved.
+//
+
+import UIKit
+
+class Drumpad : UIViewController{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        }
+    
+}
