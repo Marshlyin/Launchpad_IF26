b14fe041b4c12e14db856acf13cc4e009e088294
user148557 <user148557@92-63-141-194.rdns.melbourne.co.uk>
2018-12-31 07:24:51 -0800

    Mise en place des sons Lofi, système de try catch pour verifier le fonctionnement des sons (à voir en salle td)

--- Launchpad_IF26/DrumpadLoFi.swift
+++ Launchpad_IF26/DrumpadLoFi.swift
@@ -7,14 +7,159 @@
 //
 
 import UIKit
+import AVFoundation
 
-class Drumpad : UIViewController{
+class DrumpadLoFi: UIViewController{
     
-    
+    var Sound1 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "hat01", ofType: "wav")!)
+    var Sound2 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "hat02", ofType: "wav")!)
+    var Sound3 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "hat10", ofType: "wav")!)
+    var Sound4 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare02", ofType: "wav")!)
+    var Sound5 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare05", ofType: "wav")!)
+    var Sound6 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare09", ofType: "wav")!)
+    var Sound7 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare15", ofType: "wav")!)
+    var Sound8 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare17", ofType: "wav")!)
+    var Sound9 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare18", ofType: "wav")!)
+    var Sound10 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "kick03", ofType: "wav")!)
+    var Sound11 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "kick05", ofType: "wav")!)
+    var Sound12 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "kick11", ofType: "wav")!)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         }
+    
+    @IBAction func playSound1(_ sender: AnyObject) {
+        
+        do {
+           let audioPlayer = try AVAudioPlayer(contentsOf: Sound1 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound2(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound2 as URL)
+            audioPlayer.play()
+            print("tiptop2")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound3(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound3 as URL)
+            audioPlayer.play()
+            print("tiptop3")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound4(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound4 as URL)
+            audioPlayer.play()
+            print("tiptop4")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound5(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound5 as URL)
+            audioPlayer.play()
+            print("tiptop5")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound6(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound6 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound7(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound7 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound8(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound8 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound9(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound9 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound10(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound10 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound11(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound11 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    @IBAction func playSound12(_ sender: AnyObject) {
+        
+        do {
+            let audioPlayer = try AVAudioPlayer(contentsOf: Sound12 as URL)
+            audioPlayer.play()
+            print("tiptop")
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
     
 }
